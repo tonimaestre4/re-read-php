1435d5e01897685fe0763a971c215fe604e2ecde
tonimaestre4 <6259.joan23@fje.edu>
2020-09-30 17:13:49 +0200

    Subida

--- view/ebooks.php
+++ view/ebooks.php
@@ -6,7 +6,6 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" type="text/css" href="../CSS/estilos.css">
-<script src="../javascript/code.js"></script>
 </head>
 <body>
 
